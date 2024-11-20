4a6d04e82ab9b266f48d14ffeddcfe6276c0af02
Btomaszek <bawana2@hawaii.edu>
2024-11-19 22:57:57 -1000

    Create SocialMediaNetwork.java

---
+++ src/main/java/SocialMediaNetwork.java
@@ -0,0 +1,68 @@
+import java.util.stream.Collectors; 
+import java.util.*; 
+
+public class SocialMediaNetwork { 
+   
+   List<User> users = new ArrayList<>(); 
+   
+   public void addUser(User user) { 
+   
+      users.add(user); 
+   } 
+
+   public void addConnection(User fromUser, User toUser, String type) { 
+
+      fromUser.connections.add(new Connection(toUser, type)); 
+   } 
+
+   public void addPost(User user, Post post) { 
+   
+      user.posts.add(post); 
+   } 
+
+   public void addComment(Post post, Comment comment) { 
+
+      post.comments.add(comment); 
+      comment.user.comments.add(comment); 
+   } 
+
+   public void addView(Post post, User user) { 
+
+      post.viewers.add(user); 
+      user.viewedPosts.add(post); 
+   } // Other methods to analyze data and generate diagrams
+   
+   public DirectedMultigraph<Object, DefaultEdge> createGraph() { 
+      
+      DirectedMultigraph<Object, DefaultEdge> graph = new DirectedMultigraph<>(DefaultEdge.class); 
+      
+      // Add users and their posts as nodes 
+      for (User user : users) { graph.addVertex(user); 
+      
+         for (Post post : user.posts) { 
+            graph.addVertex(post); 
+            graph.addEdge(user, post); // Authorship 
+            
+            // Add comments as edges 
+            for (Comment comment : post.comments) { 
+               graph.addEdge(comment.user, post); 
+            } 
+            
+            // Add views as edges 
+            for (User viewer : post.viewers) { 
+               
+               graph.addEdge(viewer, post); 
+            } 
+         } 
+      } 
+      return graph; 
+   }
+   public List<User> getUsersWithMostPosts(int limit) { 
+   
+      return users.stream() 
+         .sorted((u1, u2) -> Integer.compare(u2.posts.size(), u1.posts.size())) 
+         .limit(limit) 
+         .collect(Collectors.toList()); 
+      } // Other methods for different analyses... 
+   } 
+   
